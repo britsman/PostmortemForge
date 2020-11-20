878ca98cbc2be3640d9c0e2eb5891714f62057d7
britsman <670985+britsman@users.noreply.github.com>
2020-11-20 11:43:00 +0700

    style: clean up rollback detection

--- src/postmortemforge/draft.py
+++ src/postmortemforge/draft.py
@@ -118,3 +118,11 @@ def build_draft(timeline: Timeline) -> Draft:
                     f"{metric} stayed past its threshold of {threshold:g}{unit} "
                     f"from T+{start_m:0.1f} to T+{end_m:0.1f} min."
                 ),
+                sources=_prov(interval.start_event, interval.end_event),
+            )
+        )
+
+    # Error burst extent, grounded in the first and last error of the burst.
+    for burst in timeline.bursts:
+        start_m = timeline.minutes(burst.start_ts)
+        end_m = timeline.minutes(burst.end_ts)
